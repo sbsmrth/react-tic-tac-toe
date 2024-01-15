6d2e988d0c05386473ce4542ec3f56c4670fa63d
Andrés Martínez <sbsmrth>
2024-01-15 16:43:33 -0500

    feat: method to manage board change

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,29 @@ const initialGameBoard = [
 ];
 
 export const GameBoard = () => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard(prevGameBoard => {
+      const prevGameBoardCopy = [
+        ...prevGameBoard.map(innerArr => [...innerArr]),
+      ];
+      prevGameBoardCopy[rowIndex][colIndex] = 'X';
+
+      return prevGameBoardCopy;
+    });
+  };
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
